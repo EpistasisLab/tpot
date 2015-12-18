15ccdac50cfd117a6a0cb4c74a6699c48b814861
davidmarx <david.marx@datamininglab.com>
2015-12-18 11:19:38 -0500

    Added SVM classification operator

--- tpot/operators/__init__.py
+++ tpot/operators/__init__.py
@@ -1,9 +1,11 @@
+from .C_SVM import C_SVM
 from .DecisionTree import DecisionTree
 from .KNNc import KNNc
 from .LogisticRegressionGLM import LogisticRegressionGLM
 from .RandomForest import RandomForest
 
 operator_registry = {
+    'C_SVM':C_SVM()
     'DecisionTree':DecisionTree(),
     'KNNc':KNNc(),
     'LogisticRegressionGLM':LogisticRegressionGLM(),
