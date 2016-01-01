c66bd8568f8e48f77d556b59af7b70d201133049
kadar_akos <kadar.akos@gmail.com>
2016-01-01 14:31:44 +0100

    .idea clean

--- tutorials/tpot_iris_pipeline.py
+++ tutorials/tpot_iris_pipeline.py
@@ -3,16 +3,16 @@ import pandas as pd
 
 from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.feature_selection import f_classif
-from sklearn.svm import SVC
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 # NOTE: Make sure that the class is labeled 'class' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR')
 training_indices, testing_indices = next(iter(StratifiedShuffleSplit(tpot_data['class'].values, n_iter=1, train_size=0.75, test_size=0.25)))
 
 
-# Perform classification with a C-support vector classifier
-svc2 = SVC(C=13.4)
-svc2.fit(result1.loc[training_indices].drop('class', axis=1).values, result1.loc[training_indices, 'class'].values)
-result2 = result1
-result2['svc2-classification'] = svc2.predict(result2.drop('class', axis=1).values)
+result1 = tpot_data.copy()
+
+# Perform classification with a k-nearest neighbor classifier
+knnc1 = KNeighborsClassifier(n_neighbors=min(9, len(training_indices)))
+knnc1.fit(result1.loc[training_indices].drop('class', axis=1).values, result1.loc[training_indices, 'class'].values)
+result1['knnc1-classification'] = knnc1.predict(result1.drop('class', axis=1).values)
