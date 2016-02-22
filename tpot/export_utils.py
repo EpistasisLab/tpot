a5a481c7dae65eed1a5109854d7225da5a3bedc3
Randy Olson <olsonran@msu.edu>
2016-02-22 11:20:59 -0500

    Update export_utils.py

--- tpot/export_utils.py
+++ tpot/export_utils.py
@@ -14,7 +14,7 @@ The TPOT library is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
-the Twitter Bot library. If not, see http://www.gnu.org/licenses/.
+the TPOT library. If not, see http://www.gnu.org/licenses/.
 """
 
 # Utility functions that convert the current optimized pipeline into its corresponding Python code
