cb966571bcd823d55dafc0a11f4d6e4015b6b789
Randy Olson <olsonran@msu.edu>
2016-02-22 11:20:52 -0500

    Update _version.py

--- tpot/_version.py
+++ tpot/_version.py
@@ -14,7 +14,7 @@ The TPOT library is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
-the Twitter Bot library. If not, see http://www.gnu.org/licenses/.
+the TPOT library. If not, see http://www.gnu.org/licenses/.
 """
 
 __version__ = '0.2.3'
