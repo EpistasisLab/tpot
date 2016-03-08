9a5863549c2a25acb0a88126db3acf9f6a8f78c2
Randy Olson <rso@randalolson.com>
2016-03-08 16:48:15 -0500

    Change balanced accuracy function
    Previously, the balanced accuracy function was calculating the balanced
    *sensitivity* only. Now it takes both sensitivity and specificity into
    account, and therefore calculates the balanced accuracy over all
    classes.

--- tpot/tpot.py
+++ tpot/tpot.py
@@ -1181,9 +1181,15 @@ class TPOT(object):
         all_classes = list(set(result['class'].values))
         all_class_accuracies = []
         for this_class in all_classes:
-            this_class_accuracy = len(result[(result['guess'] == this_class) \
-                & (result['class'] == this_class)])\
-                / float(len(result[result['class'] == this_class]))
+            this_class_sensitivity = len(result[(result['guess'] == this_class) &\
+                                                (result['class'] == this_class)])\
+            / float(len(result[result['class'] == this_class]))
+        
+            this_class_specificity = len(result[(result['guess'] != this_class) &\
+                                                (result['class'] != this_class)])\
+            / float(len(result[result['class'] != this_class]))
+        
+            this_class_accuracy = (this_class_sensitivity + this_class_specificity) / 2.
             all_class_accuracies.append(this_class_accuracy)
 
         balanced_accuracy = np.mean(all_class_accuracies)
