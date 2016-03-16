98ecdbbeb10aa2a42333c9fb83984b808d5ffd8b
Randy Olson <rso@randalolson.com>
2016-03-16 16:28:25 -0400

    Update version for release

--- tpot/_version.py
+++ tpot/_version.py
@@ -17,4 +17,4 @@ You should have received a copy of the GNU General Public License along with
 the TPOT library. If not, see http://www.gnu.org/licenses/.
 """
 
-__version__ = '0.2.9'
+__version__ = '0.2.10'
