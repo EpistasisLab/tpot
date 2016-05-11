0b6cbae6232eab6dd7ecb1c56a6a8ed791a9ae5e
Daniel Angell <supacoofoo@gmail.com>
2016-05-10 22:17:43 -0400

    Added Travis configuration for tqdm

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -53,6 +53,7 @@ else
 fi
 
 pip install update_checker
+pip install tqdm
 
 if [[ "$COVERAGE" == "true" ]]; then
     pip install coverage coveralls
@@ -65,6 +66,7 @@ python -c "import scipy; print('scipy %s' % scipy.__version__)"
 python -c "import sklearn; print('sklearn %s' % sklearn.__version__)"
 python -c "import pandas; print('pandas %s' % pandas.__version__)"
 python -c "import deap; print('deap %s' % deap.__version__)"
-python -c "import xgboost; print('xgboost %s ' % xgboost.__version__)"
-python -c "import update_checker; print('update_checker %s ' % update_checker.__version__)"
+python -c "import xgboost; print('xgboost %s' % xgboost.__version__)"
+python -c "import update_checker; print('update_checker %s' % update_checker.__version__)"
+python -c "import tqdm; print('tqdm %s' % tqdm.__version__)"
 python setup.py build_ext --inplace
