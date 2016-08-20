e181ceb17b4dcd203ca49d507552a1f6ac87ebc3
Randy Olson <rso@randalolson.com>
2016-08-19 22:40:09 -0400

    Remove pandas dependency from setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ or Twitter: https://twitter.com/randal_olson
 This project is hosted at https://github.com/rhiever/tpot
 ''',
     zip_safe=True,
-    install_requires=['numpy', 'scipy', 'pandas', 'scikit-learn', 'deap', 'update_checker', 'tqdm'],
+    install_requires=['numpy', 'scipy', 'scikit-learn', 'deap', 'update_checker', 'tqdm'],
     classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
