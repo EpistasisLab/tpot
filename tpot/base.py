1d982b5ed60d4307e08908000d8d69ba74292511
Daniel Angell <supacoofoo@gmail.com>
2016-09-09 18:10:24 -0400

    Clean up _set_param_recursive()

--- tpot/base.py
+++ tpot/base.py
@@ -491,12 +491,12 @@ class TPOTBase(BaseEstimator):
 
         """
         for (_, obj) in pipeline_steps:
-            if hasattr(obj, 'steps'):
-                self._set_param_recursive(obj.steps, parameter, value)
-            elif hasattr(obj, 'transformer_list'):
-                self._set_param_recursive(obj.transformer_list, parameter, value)
-            elif hasattr(obj, 'estimators'):
-                self._set_param_recursive(obj.estimators, parameter, value)
+            recursive_attrs = ['steps', 'transformer_list', 'estimators']
+
+            for attr in recursive_attrs:
+                if hasattr(obj, attr):
+                    self._set_param_recursive(getattr(obj, attr), parameter, value)
+                    break
             else:
                 if hasattr(obj, parameter):
                     setattr(obj, parameter, value)
