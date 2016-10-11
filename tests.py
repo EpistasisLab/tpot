db595b93dd0c10c2891904d51d7695cc523fdff0
Sahil Shah <deltaspin@gmail.com>
2016-10-11 14:42:40 -0400

    Test whether result matrix values are floats between 0 and 1

--- tests.py
+++ tests.py
@@ -194,6 +194,7 @@ def test_predict_2():
 
     assert result.shape == (testing_features.shape[0],)
 
+
 def test_predict_proba():
     """Assert that the TPOT predict_proba function returns a numpy matrix of shape (num_testing_rows, num_testing_classes)"""
 
@@ -206,6 +207,30 @@ def test_predict_proba():
     result = tpot_obj.predict_proba(testing_features)
 
     assert result.shape == (testing_features.shape[0], testing_classes.shape[1])
+
+
+def test_predict_proba2():
+    """Assert that the TPOT predict_proba function returns a numpy matrix filled with probabilities (float)"""
+
+    tpot_obj = TPOTClassifier()
+    tpot_obj._optimized_pipeline = creator.Individual. \
+        from_string('DecisionTreeClassifier(input_matrix)', tpot_obj._pset)
+    tpot_obj._fitted_pipeline = tpot_obj._toolbox.compile(expr=tpot_obj._optimized_pipeline)
+    tpot_obj._fitted_pipeline.fit(training_features, training_classes)
+
+    result = tpot_obj.predict_proba(testing_features)
+
+    rows = testing_features.shape[0]
+    columns = testing_classes.shape[1]
+
+    try:
+        for i in range(rows):
+            for j in range(columns):
+                float_range(result[i][j])
+        assert True
+    except Exception:
+        assert False
+
 
 def test_fit():
     """Assert that the TPOT fit function provides an optimized pipeline"""
