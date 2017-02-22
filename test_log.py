7bc9044d690d55087f392f763e43c437c5ab1191
Weixuan Fu <weixuanf@mail.med.upenn.edu>
2017-02-22 09:40:15 -0500

    pbar still not work need fix around (test_log.py)

--- test_log.py
+++ test_log.py
@@ -1,53 +1,41 @@
-# coding: utf-8
-get_ipython().magic('load tpot_test_multi_process.py')
-# %load tpot_test_multi_process.py
-from tpot import TPOTClassifier
 from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.base import BaseEstimator, clone
+from sklearn.externals.joblib import Parallel, delayed
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 import time
+from tqdm import tqdm
+
 
 digits = load_digits()
 X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target,
                                                     train_size=0.25, test_size=0.75)
 
-#tpot = TPOTClassifier(generations=3, population_size=10, verbosity=2, num_cpu=1, random_state = 42)
-#time_start = time.time()
-#tpot.fit(X_train, y_train)
-#print(tpot.score(X_test, y_test))
-#print('\nTime used with num_cpu = 1:',time.time()-time_start)
 
-tpot = TPOTClassifier(generations=2, population_size=10, verbosity=2, max_eval_time_mins=0.02, n_jobs = 2, random_state = 42)
-time_start = time.time()
-tpot.fit(X_train, y_train)
-print(tpot.score(X_test, y_test))
-print('\nTime used with num_cpu = 3:',time.time()-time_start)
-tpot.sklearn_pipeline_list[0]
-from sklearn.model_selection import cross_val_score
-from sklearn.base import BaseEstimator, clone
-from sklearn.externals.joblib import Parallel, delayed
-parallel = Parallel(n_jobs=self.n_jobs)
+pbar = tqdm(total=10, unit='pipeline', leave=False,
+                  disable=False, desc='Optimization Progress')
+
+pipeline = make_pipeline(StandardScaler(), GaussianNB(priors=None))
+
+sklearn_pipelines = [pipeline]*10
+
+def _wrapped_cross_val_score(sklearn_pipeline, features, classes,
+cv, pbar):
+    cv_scores = cross_val_score(sklearn_pipeline, features, classes,
+        cv=cv, n_jobs=1)
+    pbar.update(1)
+    return cv_scores
+
 parallel = Parallel(n_jobs=2)
-resulting_score_list = parallel(delayed(cross_val_score)(clone(sklearn_pipeline),
-            features=X, classes=y, cv=3, njobs = 1)
-            for sklearn_pipeline in sklearn_pipeline_list)
-resulting_score_list = parallel(delayed(cross_val_score)(clone(sklearn_pipeline),
-            features=X, classes=y, cv=3, njobs = 1)
-            for sklearn_pipeline in tpot.sklearn_pipeline_list)
-resulting_score_list = parallel(delayed(cross_val_score)(clone(sklearn_pipeline),
-            features=X_train, classes=y_train, cv=3, njobs = 1)
-            for sklearn_pipeline in tpot.sklearn_pipeline_list)
-resulting_score_list = parallel(delayed(cross_val_score)(clone(sklearn_pipeline),
-            X=X_train, y=y_train, cv=3, njobs = 1)
-            for sklearn_pipeline in tpot.sklearn_pipeline_list)
-resulting_score_list = parallel(delayed(cross_val_score)(clone(sklearn_pipeline),
-            X=X_train, y=y_train, cv=3, n_jobs = 1)
-            for sklearn_pipeline in tpot.sklearn_pipeline_list)
-partial_cross_val_score = partial(cross_val_score, X=X_train, y=y_train,
-            cv=5, n_jobs=1)
+resulting_score_list = parallel(delayed(_wrapped_cross_val_score)(clone(sklearn_pipeline),
+            features=X_train, classes=y_train, cv=3, pbar = pbar)
+            for sklearn_pipeline in sklearn_pipelines)
+
 from functools import partial
 from pathos.multiprocessing import Pool
-partial_cross_val_score = partial(cross_val_score, X=X_train, y=y_train,
-            cv=5, n_jobs=1)
+partial_cross_val_score = partial(_wrapped_cross_val_score, features=X_train, classes=y_train, cv=3, pbar = pbar)
 pool = Pool(processes=2)
-resulting_score_list = pool.map(partial_cross_val_score, tpot.sklearn_pipeline_list)
+resulting_score_list = pool.map(partial_cross_val_score, sklearn_pipelines)
 resulting_score_list
