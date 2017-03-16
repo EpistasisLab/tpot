a9383208d47eda9bdade6e6340756c24c685ce31
Pieter Gijsbers <p.gijsbers@student.tue.nl>
2017-03-16 16:06:20 +0100

    Allow for missing values

--- tpot/base.py
+++ tpot/base.py
@@ -302,7 +302,8 @@ class TPOTBase(BaseEstimator):
 
         # Terminals
         for _type in self.arguments:
-            for val in _type.values:
+            type_values = list(_type.values) + ['MISSING']
+            for val in type_values:
                 terminal_name = _type.__name__ + "=" + str(val)
                 self._pset.addTerminal(val, _type, name=terminal_name)
 
