ce6a9fbd0c197d18198201fe6b2fd7562e26ce1f
sohnam <ansohn88@gmail.com>
2017-04-17 10:14:06 -0400

    Update tpot_mdr_classifier.py

--- tpot/tpot_mdr_classifier.py
+++ tpot/tpot_mdr_classifier.py
@@ -4,6 +4,13 @@ classifier_config_dict = {
 
     # Classifiers
     
+    'mdr.MDR': {
+    'tie_break': tie_break % 2
+    'default_label': default_label % 2
+    },
+    
+    # Feature Selectors
+    
     'skrebate.ReliefF': {
         'n_features_to_select': max(1, n_features_to_select)
     },
