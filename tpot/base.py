d9c18635bf89ca8a7555b9a11ba5585a67691296
Weixuan Fu <weixuanf@mail.med.upenn.edu>
2017-05-01 17:34:40 -0400

    threading

--- tpot/base.py
+++ tpot/base.py
@@ -27,7 +27,7 @@ import sys
 from functools import partial
 from datetime import datetime
 from multiprocessing import cpu_count
-from dask import compute, delayed, multiprocessing
+from dask import compute, delayed, multiprocessing, threaded
 
 import numpy as np
 import deap
@@ -794,7 +794,8 @@ class TPOTBase(BaseEstimator):
                                                                     sample_weight,
                                                                     timeout=self.max_eval_time_seconds)
                       for sklearn_pipeline in sklearn_pipeline_list[chunk_idx:chunk_idx+self.n_jobs*4]]
-            tmp_scores = compute(*pre_tmp_scores, get=multiprocessing.get, num_workers=self.n_jobs)
+            #tmp_scores = compute(*pre_tmp_scores, get=multiprocessing.get, num_workers=self.n_jobs)
+            tmp_scores = compute(*pre_tmp_scores, get=threaded.get, num_workers=self.n_jobs)
             for val in tmp_scores:
                 if not self._pbar.disable:
                     self._pbar.update(1)
