a028d017e30b563096f12cb48b3df424e00d349e
Weixuan Fu <weixuanf@mail.med.upenn.edu>
2017-05-20 21:15:49 -0400

    add StackingEstimator

--- tpot/built_in_operators.py
+++ tpot/built_in_operators.py
@@ -20,11 +20,11 @@ License along with TPOT. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import numpy as np
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, TransformerMixin, clone
 from sklearn.utils import check_array
 
 
-class ZeroCount(BaseEstimator):
+class ZeroCount(BaseEstimator, TransformerMixin):
     """Adds the count of zeros and count of non-zeros per sample as features."""
 
     def fit(self, X, y=None):
@@ -76,3 +76,57 @@ class CombineDFs(object):
     def __name__(self):
         """Instance name is the same as the class name."""
         return self.__class__.__name__
+
+
+class StackingEstimator(BaseEstimator, TransformerMixin):
+    """Meta-transformer for adding predictions and/or class probabilities
+    as synthetic feature(s)
+    Parameters
+    ----------
+    estimator : object
+        The base estimator from which the transformer is built.
+    """
+    def __init__(self, estimator):
+        self.estimator = estimator
+
+    def fit(self, X, y=None, **fit_params):
+        """Fit the StackingEstimator meta-transformer.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            The training input samples.
+        y : array-like, shape (n_samples,)
+            The target values (integers that correspond to classes in
+            classification, real numbers in regression).
+        **fit_params : Other estimator specific parameters
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
+        self.estimator.fit(X, y, **fit_params)
+        return self
+
+    def transform(self, X, y=None):
+        """Transform data by adding two virtual features.
+
+        Parameters
+        ----------
+        X: numpy ndarray, {n_samples, n_components}
+            New data, where n_samples is the number of samples and n_components
+            is the number of components.
+        y: None
+            Unused
+
+        Returns
+        -------
+        X_transformed: array-like, shape (n_samples, n_features + 1)
+            or (n_samples, n_features + 2) for classifier with predict_proba attribute
+            The transformed feature set.
+        """
+        # add class probabilities as a synthetic feature
+        if issubclass(self.estimator, ClassifierMixin) and hasattr(self.estimator, 'predict_proba'):
+            X = np.hstack(np.reshape(self.estimator.predict_proba(X), (-1,1)), X)
+        # add class prodiction as a synthetic feature
+        X_transformed = np.hstack(np.reshape(self.estimator.predict(X), (-1,1)), X)
+        return X_transformed
