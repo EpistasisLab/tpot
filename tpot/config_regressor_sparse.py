fc6cf0a612f2041115fda2e48118a9f6839db4c2
Randy Olson <rso@randalolson.com>
2017-05-24 15:23:31 -0400

    Update config_regressor_sparse.py

--- tpot/config_regressor_sparse.py
+++ tpot/config_regressor_sparse.py
@@ -22,6 +22,7 @@ import numpy as np
 
 regressor_config_sparse = {
     'tpot.one_hot_encoder.OneHotEncoder': {
+        'minimum_fraction': [0.05, 0.1, 0.15, 0.2, 0.25]
     },
 
     'sklearn.neighbors.KNeighborsRegressor': {
