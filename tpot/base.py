251b0352ad1850272951259e1a5aed3c06a4262d
Randy Olson <rso@randalolson.com>
2017-06-01 18:05:56 -0400

    Extraneous if statement removed

--- tpot/base.py
+++ tpot/base.py
@@ -574,14 +574,13 @@ class TPOTBase(BaseEstimator):
                                 print('Best pipeline: {}'.format(self._optimized_pipeline))
 
                             # Store and fit the entire Pareto front as fitted models for convenience
-                            if self._pareto_front:
-                                self.pareto_front_fitted_pipelines_ = {}
+                            self.pareto_front_fitted_pipelines_ = {}
 
-                                for pipeline in self._pareto_front.items:
-                                    self.pareto_front_fitted_pipelines_[str(pipeline)] = self._toolbox.compile(expr=pipeline)
-                                    with warnings.catch_warnings():
-                                        warnings.simplefilter('ignore')
-                                        self.pareto_front_fitted_pipelines_[str(pipeline)].fit(features, target)
+                            for pipeline in self._pareto_front.items:
+                                self.pareto_front_fitted_pipelines_[str(pipeline)] = self._toolbox.compile(expr=pipeline)
+                                with warnings.catch_warnings():
+                                    warnings.simplefilter('ignore')
+                                    self.pareto_front_fitted_pipelines_[str(pipeline)].fit(features, target)
                     break
 
                 except (KeyboardInterrupt, SystemExit, Exception) as e:
