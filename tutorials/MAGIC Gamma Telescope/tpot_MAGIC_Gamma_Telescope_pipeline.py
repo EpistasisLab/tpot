223057570553fc781e03637c9dd3b9ea4115157d
Hristo Georgiev <hristo.a.georgiev@gmail.com>
2017-10-24 23:04:48 +0100

    Update tutorial notebooks and generated pipelines

--- tutorials/MAGIC Gamma Telescope/tpot_MAGIC_Gamma_Telescope_pipeline.py
+++ tutorials/MAGIC Gamma Telescope/tpot_MAGIC_Gamma_Telescope_pipeline.py
@@ -1,17 +1,22 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.tree import DecisionTreeClassifier
+from tpot.builtins import StackingEstimator
 
-from sklearn.cross_validation import train_test_split
-from sklearn.ensemble import GradientBoostingClassifier
+# NOTE: Make sure that the class is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1).values
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# NOTE: Make sure that the class is labeled 'class' in the data file
-tpot_data = pd.read_csv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR')
-training_indices, testing_indices = train_test_split(tpot_data.index, stratify = tpot_data['class'].values, train_size=0.75, test_size=0.25)
+# Score on the training set was:0.853347788745
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=LogisticRegression(C=10.0, dual=False, penalty="l2")),
+    DecisionTreeClassifier(criterion="gini", max_depth=7, min_samples_leaf=5, min_samples_split=7)
+)
 
-result1 = tpot_data.copy()
-
-# Perform classification with a gradient boosting classifier
-gbc1 = GradientBoostingClassifier(learning_rate=0.49, max_features=1.0, min_weight_fraction_leaf=0.09, n_estimators=500, random_state=42)
-gbc1.fit(result1.loc[training_indices].drop('class', axis=1).values, result1.loc[training_indices, 'class'].values)
-
-result1['gbc1-classification'] = gbc1.predict(result1.drop('class', axis=1).values)
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
