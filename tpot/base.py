7e3dfffcb0e82d5343443a1582fecbd69f43bdf1
weixuanfu2016 <weixuanf@pennmedicine.upenn.edu>
2017-11-17 12:07:55 -0500

    support for py27

--- tpot/base.py
+++ tpot/base.py
@@ -359,7 +359,10 @@ class TPOTBase(BaseEstimator):
                 # Heuristic to ensure user has not passed a metric
                 module = getattr(scoring, '__module__', None)
                 if sys.version_info[0] < 3:
-                    args_list = inspect.getargspec(scoring)[0]
+                    if not inspect.isclass(scoring)
+                        args_list = inspect.getargspec(scoring)[0]
+                    else:
+                        args_list = ['NA', 'NA']
                 else:
                     args_list = inspect.getfullargspec(scoring)[0]
                 if args_list == ["y_true", "y_pred"] or (hasattr(module, 'startswith') and \
