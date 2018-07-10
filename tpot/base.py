77b2b32c51d9b8bb52f55efe2736a46cd5d42f9c
weixuanfu2016 <weixuanf@pennmedicine.upenn.edu>
2018-07-10 14:52:42 -0400

    simply is_notebook

--- tpot/base.py
+++ tpot/base.py
@@ -103,10 +103,9 @@ def is_notebook():
     try:
         from IPython import get_ipython
         shell = get_ipython().__class__.__name__
+        # if shell == 'TerminalInteractiveShell', then Terminal running IPython
         if shell == 'ZMQInteractiveShell':
             return True   # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return False  # Terminal running IPython
         else:
             return False  # Other type
     except:
