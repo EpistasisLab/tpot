2577d08acf86e1b825425b9949d22f64a1b62b95
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-08-15 12:58:04 -0500

    some cleanup, tests

--- tests/test_dask_based.py
+++ tests/test_dask_based.py
@@ -16,23 +16,30 @@ except ImportError:
     raise nose.SkipTest()
 
 
-
 class TestDaskMatches(unittest.TestCase):
     def test_dask_matches(self):
-        a = TPOTClassifier(
-            generations=2,
-            population_size=5,
-            cv=3,
-            random_state=0,
-            use_dask=False,
-        )
-        b = TPOTClassifier(
-            generations=2,
-            population_size=5,
-            cv=3,
-            random_state=0,
-            use_dask=True,
-        )
-        X, y = make_classification(random_state=0)
-        a.fit(X, y)
-        b.fit(X, y)
+        for n_jobs in [1, -1]:
+            X, y = make_classification(random_state=0)
+            a = TPOTClassifier(
+                generations=2,
+                population_size=5,
+                cv=3,
+                random_state=0,
+                n_jobs=-1,
+                use_dask=False,
+            )
+            b = TPOTClassifier(
+                generations=2,
+                population_size=5,
+                cv=3,
+                random_state=0,
+                n_jobs=-1,
+                use_dask=True,
+            )
+            b.fit(X, y)
+            a.fit(X, y)
+            self.assertEqual(a.evaluated_individuals_,
+                             b.evaluated_individuals_)
+            self.assertEqual(a.pareto_front_fitted_pipelines_.keys(),
+                             b.pareto_front_fitted_pipelines_.keys())
+            self.assertEqual(a.score(X, y), b.score(X, y))
