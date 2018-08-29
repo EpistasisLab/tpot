d9b4d9a7319eea88e8ae106badbbe60a9bc7279a
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-08-29 08:29:35 -0500

    Try single-threaded

--- tests/test_dask_based.py
+++ tests/test_dask_based.py
@@ -20,32 +20,33 @@ class TestDaskMatches(unittest.TestCase):
     maxDiff = None
 
     def test_dask_matches(self):
-        for n_jobs in [1, -1]:
-            X, y = make_classification(random_state=0)
-            a = TPOTClassifier(
-                generations=2,
-                population_size=5,
-                cv=3,
-                random_state=0,
-                n_jobs=n_jobs,
-                use_dask=False,
-            )
-            b = TPOTClassifier(
-                generations=2,
-                population_size=5,
-                cv=3,
-                random_state=0,
-                n_jobs=n_jobs,
-                use_dask=True,
-            )
-            b.fit(X, y)
-            a.fit(X, y)
+        with dask.config.set(scheduler='single-threaded'):
+            for n_jobs in [-1]:
+                X, y = make_classification(random_state=0)
+                a = TPOTClassifier(
+                    generations=2,
+                    population_size=5,
+                    cv=3,
+                    random_state=0,
+                    n_jobs=n_jobs,
+                    use_dask=False,
+                )
+                b = TPOTClassifier(
+                    generations=2,
+                    population_size=5,
+                    cv=3,
+                    random_state=0,
+                    n_jobs=n_jobs,
+                    use_dask=True,
+                )
+                b.fit(X, y)
+                a.fit(X, y)
 
-            self.assertEqual(a.score(X, y), b.score(X, y))
-            self.assertEqual(a.pareto_front_fitted_pipelines_.keys(),
-                             b.pareto_front_fitted_pipelines_.keys())
-            self.assertEqual(a.evaluated_individuals_,
-                             b.evaluated_individuals_)
+                self.assertEqual(a.score(X, y), b.score(X, y))
+                self.assertEqual(a.pareto_front_fitted_pipelines_.keys(),
+                                b.pareto_front_fitted_pipelines_.keys())
+                self.assertEqual(a.evaluated_individuals_,
+                                b.evaluated_individuals_)
 
     def test_handles_errors(self):
         X, y = make_classification(n_samples=5)
