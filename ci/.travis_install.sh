1cfcbd7171c5e52c934f6a3f5ae16b0e56e336de
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2018-11-01 10:47:23 -0400

    fix conda env in ci

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -23,10 +23,10 @@ deactivate
 
 # Use the miniconda installer for faster download / install of conda
 # itself
-wget https://repo.continuum.io/miniconda/Miniconda2-4.5.11-Linux-x86_64.sh \
+wget https://repo.continuum.io/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh \
     -O miniconda.sh
 chmod +x miniconda.sh && ./miniconda.sh -b
-export PATH=/home/travis/miniconda/bin:$PATH
+export PATH=/home/travis/miniconda3/bin:$PATH
 conda update --yes conda
 
 # Configure the conda environment and put it in the path using the
@@ -54,7 +54,6 @@ pip install update_checker
 pip install tqdm
 pip install stopit
 pip install xgboost
-pip install cloudpickle
 pip install dask[delayed]
 pip install dask-ml
 
