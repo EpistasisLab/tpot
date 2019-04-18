b0a3890a7f9c7e7480a134d5ac152002cbe625ec
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2019-04-18 14:36:55 -0400

    refine travis ci

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -26,7 +26,7 @@ deactivate
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
     -O miniconda.sh
 chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda3/bin:$PATH"
+export PATH="$HOME/miniconda/bin:$PATH"
 conda update --yes conda
 
 # Configure the conda environment and put it in the path using the
