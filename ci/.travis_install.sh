c644c657b97f08fa7ce1ec7e0e4fdd4fac9da992
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2019-04-18 15:12:00 -0400

    refine macos ci test

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -23,8 +23,7 @@ export CXX=g++
 
 # Use the miniconda installer for faster download / install of conda
 # itself
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-    -O miniconda.sh
+
 chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda update --yes conda
