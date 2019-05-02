d9d3f795829c5f0bd09f89873bc5cbc5c243588f
Jan-Hendrik Menke <mail@jhmenke.de>
2019-05-02 14:30:05 +0200

    add catboost in default config if available

--- tpot/config/regressor.py
+++ tpot/config/regressor.py
@@ -24,6 +24,7 @@ License along with TPOT. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import numpy as np
+import sys
 
 # Check the TPOT documentation for information on the structure of config dicts
 
@@ -195,3 +196,11 @@ regressor_config_dict = {
     }
 
 }
+
+if 'catboost' in sys.modules.keys():
+    from sklearn.base import RegressorMixin
+    from catboost import CatBoostRegressor
+    CatBoostRegressor.__bases__ += (RegressorMixin,)
+    regressor_config_dict['catboost.CatBoostRegressor'] = {
+        'logging_level': ['Silent']
+    }
