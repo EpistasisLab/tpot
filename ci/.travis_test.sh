fdf8eb38588c8307c0fb1bb81b7e50a895db3329
James Myatt <james.myatt@tessella.com>
2019-05-16 16:57:35 +0100

    Update CI scripts

--- ci/.travis_test.sh
+++ ci/.travis_test.sh
@@ -13,6 +13,7 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 python -c "import sklearn; print('sklearn %s' % sklearn.__version__)"
+python -c "import joblib; print('joblib %s' % joblib.__version__)"
 python -c "import deap; print('deap %s' % deap.__version__)"
 python -c "import xgboost; print('xgboost %s ' % xgboost.__version__)"
 python -c "import update_checker; print('update_checker %s ' % update_checker.__version__)"
