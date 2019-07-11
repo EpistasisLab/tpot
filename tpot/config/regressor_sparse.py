56be8fe2a477f593309c380716f2ee58e95490a2
Christian Römer <croemer@informationsfabrik.de>
2019-07-11 14:33:36 +0200

    Add explicit XGBoostRegressor objective to avoid deprecation error

--- tpot/config/regressor_sparse.py
+++ tpot/config/regressor_sparse.py
@@ -94,5 +94,6 @@ regressor_config_sparse = {
         'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
         'subsample': np.arange(0.05, 1.01, 0.05),
         'min_child_weight': range(1, 21),
-        'nthread': [1]
+        'nthread': [1],
+        'objective': ['reg:squarederror']
     }}
