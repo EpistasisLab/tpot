e75694ec0fd61a78c7c595cf8e679ebc0436149f
Jose <jgh9094@gmail.com>
2023-10-30 10:54:32 -0700

    passed rng to control mutations/crossover and initial set generation

--- tpot2/individual_representations/subset_selector/subsetselector.py
+++ tpot2/individual_representations/subset_selector/subsetselector.py
@@ -1,14 +1,17 @@
 from numpy import iterable
 import tpot2
-import random
+import numpy as np
 from .. import BaseIndividual
 
 class SubsetSelector(BaseIndividual):
     def __init__(   self,
                     values,
+                    rng_,
                     initial_set = None,
                     k=1, #step size for shuffling
                 ):
+
+        rng = np.random.default_rng(rng_)
 
         if isinstance(values, int):
             self.values = set(range(0,values))
@@ -17,7 +20,7 @@ class SubsetSelector(BaseIndividual):
 
 
         if initial_set is None:
-            self.subsets = set(random.choices(values, k=k))
+            self.subsets = set(rng.choices(values, k=k))
         else:
             self.subsets = set(initial_set)
 
@@ -25,20 +28,23 @@ class SubsetSelector(BaseIndividual):
 
         self.mutation_list = [self._mutate_add, self._mutate_remove]
         self.crossover_list = [self._crossover_swap]
-        
-    def _mutate_add(self,):
+
+    def _mutate_add(self, rng_):
+        rng = np.random.default_rng(rng_)
         not_included = list(self.values.difference(self.subsets))
         if len(not_included) > 1:
-            self.subsets.update(random.sample(not_included, k=min(self.k, len(not_included))))
+            self.subsets.update(rng.choice(not_included, k=min(self.k, len(not_included))))
             return True
         else:
             return False
 
-    def _mutate_remove(self,):
+    def _mutate_remove(self, rng_):
+        rng = np.random.default_rng(rng_)
         if len(self.subsets) > 1:
-            self.subsets = self.subsets - set(random.sample(list(self.subsets), k=min(self.k, len(self.subsets)-1) ))
+            self.subsets = self.subsets - set(rng.choice(list(self.subsets), k=min(self.k, len(self.subsets)-1) ))
 
-    def _crossover_swap(self, ss2):
+    def _crossover_swap(self, ss2, rng_):
+        rng = np.random.default_rng(rng_)
         diffs = self.subsets.symmetric_difference(ss2.subsets)
 
         if len(diffs) == 0:
@@ -46,6 +52,6 @@ class SubsetSelector(BaseIndividual):
         for v in diffs:
             self.subsets.discard(v)
             ss2.subsets.discard(v)
-            random.choice([self.subsets, ss2.subsets]).add(v)
-        
+            rng.choice([self.subsets, ss2.subsets]).add(v)
+
         return True
