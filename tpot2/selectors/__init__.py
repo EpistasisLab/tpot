a007c13bbdee97cd84df85f5c1fcea7047ec30c2
perib <pedroribeiroh@gmail.com>
2024-05-16 16:25:49 -0700

    import map elites selectors

--- tpot2/selectors/__init__.py
+++ tpot2/selectors/__init__.py
@@ -4,6 +4,7 @@ from .random_selector import random_selector
 from .tournament_selection import tournament_selection
 from .tournament_selection_dominated import tournament_selection_dominated
 from .nsgaii import nondominated_sorting, crowding_distance, dominates, survival_select_NSGA2
+from .map_elites_selection import map_elites_survival_selector, map_elites_parent_selector
 
 
 SELECTORS =     {"lexicase":lexicase_selection,
@@ -12,4 +13,6 @@ SELECTORS =     {"lexicase":lexicase_selection,
                 "tournament":tournament_selection,
                 "tournament_dominated":tournament_selection_dominated,
                 "nsgaii":survival_select_NSGA2,
+                "map_elites_survival":map_elites_survival_selector,
+                "map_elites_parent":map_elites_parent_selector,
                 }
