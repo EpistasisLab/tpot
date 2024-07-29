90498c7c1bfa1389b2d4109029347e48dacce8d3
gketron <gketron@uci.edu>
2024-07-29 14:04:54 -0700

    Conditional Sample Posterior Added for Iterative Imputer

--- tpot2/config/imputers.py
+++ tpot2/config/imputers.py
@@ -4,6 +4,7 @@ import sklearn.linear_model
 import sklearn.neighbors
 from ConfigSpace import ConfigurationSpace
 from ConfigSpace import ConfigurationSpace, Integer, Float, Categorical, Normal
+from ConfigSpace import EqualsCondition
 
 
 simple_imputer_cs = ConfigurationSpace(
@@ -18,33 +19,35 @@ simple_imputer_cs = ConfigurationSpace(
 )
 
 def get_IterativeImputer_config_space(n_features, random_state):
-    space = { 
-            'estimator' : Categorical('estimator', 
-                                      ['Bayesian', 'RFR', 'Ridge', 
-                                       'KNN']),
-            'sample_posterior' : Categorical('sample_posterior', [True, False]),
-            'initial_strategy' : Categorical('initial_strategy', 
+    space = { 'initial_strategy' : Categorical('initial_strategy', 
                                              ['mean', 'median', 
                                               'most_frequent', 'constant']),
-            'n_nearest_features' : Integer('n_nearest_features', 
+                'n_nearest_features' : Integer('n_nearest_features', 
                                            bounds=(1, n_features)),
-            'imputation_order' : Categorical('imputation_order', 
+                'imputation_order' : Categorical('imputation_order', 
                                              ['ascending', 'descending', 
                                               'roman', 'arabic', 'random']),
     }
+
+    estimator = Categorical('estimator', ['Bayesian', 'RFR', 'Ridge', 'KNN'])  
+    sample_posterior = Categorical('sample_posterior', [True, False])
+    sampling_condition = EqualsCondition(sample_posterior, estimator, 'Bayesian')
+
     if random_state is not None: 
             #This is required because configspace doesn't allow None as a value
             space['random_state'] = random_state
 
-    return ConfigurationSpace(
-            space = space
-            )
+    cs = ConfigurationSpace(space=space)
+    cs.add_hyperparameters([estimator, sample_posterior])
+    cs.add_conditions([sampling_condition])
+    return cs
 
 def get_KNNImputer_config_space(n_samples):
     space = {
             'n_neighbors': Integer('n_neighbors', bounds=(1, max(n_samples,100))),
             'weights': Categorical('weights', ['uniform', 'distance'])
     }
+
     return ConfigurationSpace(
           space=space
     )
@@ -54,26 +57,24 @@ def IterativeImputer_hyperparameter_parser(params):
     match est:
         case 'Bayesian':
                 estimator = sklearn.linear_model.BayesianRidge()
-                posterior = params['sample_posterior']
         case 'RFR':
                 estimator = sklearn.ensemble.RandomForestRegressor()
-                posterior = False
         case 'Ridge':
                 estimator = sklearn.linear_model.Ridge()
-                posterior = False
         case 'KNN':
                 estimator = sklearn.neighbors.KNeighborsRegressor()
-                posterior = False
-    
+
     final_params = {
             'estimator' : estimator,
-            'sample_posterior' : posterior,
             'initial_strategy' : params['initial_strategy'],
             'n_nearest_features' : params['n_nearest_features'],
             'imputation_order' : params['imputation_order'],
     }
 
-    if "random_state" in params:
-        final_params['random_state'] = params['random_state']
+    if 'sample_posterior' in params:
+        final_params['sample_posterior'] = params['sample_posterior']
 
+    if 'random_state' in params:
+        final_params['random_state'] = params['random_state']
+        
     return final_params
