698dd761f87fa39f27bdda98d368f631a21762c4
perib <pedroribeiroh@gmail.com>
2024-10-08 11:37:46 -0700

    edit old config space converter

--- tpot2/old_config_utils/old_config_utils.py
+++ tpot2/old_config_utils/old_config_utils.py
@@ -3,7 +3,7 @@ from ConfigSpace import ConfigurationSpace, Integer, Float, Categorical, Normal
 from ConfigSpace import EqualsCondition, OrConjunction, NotEqualsCondition, InCondition
 
 from ..search_spaces.nodes import EstimatorNode
-from ..search_spaces.pipelines import WrapperPipeline, ChoicePipeline, GraphSearchPipeline
+from ..search_spaces.pipelines import WrapperPipeline, ChoicePipeline, GraphSearchPipeline, SequentialPipeline, DynamicLinearPipeline
 import ConfigSpace
 import sklearn
 from functools import partial
@@ -69,7 +69,7 @@ def get_node_space(module_string, params):
             param = list(param)
 
         if isinstance(param, list) or isinstance(param, np.ndarray):
-            if len(param) == 0:
+            if len(param) == 1:
                 p = param[0]
                 config_space.add(ConfigSpace.hyperparameters.Constant(param_name, p))
             else:
@@ -203,4 +203,49 @@ def convert_config_dict_to_graphpipeline(config_dict):
     inner_space = ChoicePipeline(inner_search_spaces)
 
     final_space = GraphSearchPipeline(root_search_space=root_space, inner_search_space=inner_space)
+    return final_space
+
+
+#Note doesn't convert estimators so they passthrough inputs like in TPOT1
+def convert_config_dict_to_linearpipeline(config_dict):
+    """
+    Takes in a TPOT2 config dictionary and returns a GraphSearchPipeline search space that represents the config_dict.
+    This space will sample from all included modules in the config_dict. It will also identify classifiers/regressors to set the search space for the root node.
+
+    Note doesn't convert estimators so they passthrough inputs like in TPOT1
+    Parameters
+    ----------
+    config_dict : dict
+        The dictionary representation of the TPOT2 config.
+    
+    Returns
+    -------
+    GraphSearchPipeline
+        A GraphSearchPipeline search space that represents the config_dict.
+    """
+    root_search_spaces = []
+    inner_search_spaces = []
+
+    for key, value in config_dict.items():
+        #if root
+        if issubclass(load_get_module_from_string(key), sklearn.base.ClassifierMixin) or issubclass(load_get_module_from_string(key), sklearn.base.RegressorMixin):
+            root_search_spaces.append(get_node_space(key, value))
+        else:
+            inner_search_spaces.append(get_node_space(key, value))
+        
+    if len(root_search_spaces) == 0:
+        Warning("No classifiers or regressors found, allowing any estimator to be the root node")
+        root_search_spaces = inner_search_spaces
+
+    #merge inner and root search spaces
+
+    inner_space = np.concatenate([root_search_spaces,inner_search_spaces])
+
+    root_space = ChoicePipeline(root_search_spaces)
+    inner_space = ChoicePipeline(inner_search_spaces)
+
+    final_space = SequentialPipeline([
+        DynamicLinearPipeline(inner_space, 10),
+        root_space
+    ])
     return final_space
